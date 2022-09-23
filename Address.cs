4ead06cf1c92e267dd590bee5de0ce77f5f88f20
Fabio Moro <fabio.moro2000@gmail.com>
2022-09-23 14:54:17 +0200

    Aggiungere i file di progetto.

---
+++ Address.cs
@@ -0,0 +1,26 @@
+﻿public class Address
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Province { get; set; }
+    public string Zip { get; set; }
+
+    public Address(string name, string surname, string street, string city, string province, string zip)
+    {
+        Name = name;
+        Surname = surname;
+        Street = street;
+        City = city;
+        Province = province;
+        Zip = zip;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Name: "+ Name +", Surname: "+ Surname +", Street: "+ Street +", City: "+ City + ", Province: "+ Province +", Zip: "+ Zip);
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Linq.Expressions;
+
+// open stream on file
+StreamReader AddressesFile = File.OpenText("C:/Users/81N800KVIX/Desktop/addresses.csv");
+
+// skip first line
+AddressesFile.ReadLine();
+
+// while there are more lines
+while (!AddressesFile.EndOfStream)
+{
+    string name, surname, street, city, province, zip;
+
+    // get current line
+    string line = AddressesFile.ReadLine();
+
+    // split string every ','
+    string[] split = line.Split(',');
+
+    try
+    {
+        name = split[0];
+        surname = split[1];
+        street = split[2];
+        city = split[3];
+        province = split[4];
+        zip = split[5];
+
+        // if street doesn't begin with a number
+        if (!Char.IsDigit(street[0]))
+        {
+            throw new FormatException();
+        }
+
+        if(name == "" | surname == "" | street == "" | city == "" | province == "" | zip == "")
+        {
+            throw new FormatException();
+        }
+
+        // create instance of address object
+        Address address = new Address(name, surname, street, city, province, zip);
+
+        // print address info
+        address.Print();
+        
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("Invalid Address: " + line);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid Format: " + line);
+    }
+}
